5f20f346ed58267c3e590b82d679b4000166ae62
Mairah <animefreaknor@hotmail.com>
2019-07-21 21:10:43 +1000

    Added account route

--- Workshop3/routes/accountroute.js
+++ Workshop3/routes/accountroute.js
@@ -1,6 +1,6 @@
 module.exports = function(app,path){
-    app.get('/login', function(req,res){
-        let filepath = path.resolve('./www/index.html');
+    app.get('/account', function(req,res){
+        let filepath = path.resolve('./www/account.html');
         res.sendFile(filepath);
     });
  }
